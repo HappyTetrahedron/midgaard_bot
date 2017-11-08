287bb6b379833a10a1eca80a5fbe52422e345c76
Javier Sancho <jsf@jsancho.org>
2017-11-08 17:29:50 +0100

    Managing sessions functionality

--- sessions.go
+++ sessions.go
@@ -8,3 +8,23 @@ type Session struct {
 	Chat *tgbotapi.Chat
 	Msgbox chan tgbotapi.Message
 }
+
+var sessions map[int64]*Session
+
+func initSessions() {
+	sessions = make(map[int64]*Session)
+}
+
+func getSession(chat *tgbotapi.Chat) *Session {
+	session, ok := sessions[chat.ID]
+	if !ok {
+		session = newSession(chat)
+	}
+	return session
+}
+
+func newSession(chat *tgbotapi.Chat) *Session {
+	session := Session{chat, make(chan tgbotapi.Message)}
+	sessions[chat.ID] = &session
+	return &session
+}
